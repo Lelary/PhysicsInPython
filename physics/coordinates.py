05dec4849f732bc311e7c290247cc3d0b3c10e96
Lelary <lelary@outlook.com>
2017-12-18 23:47:47 +0900

    converting cartesian, polar, cylindrical, spherical coordinates

---
+++ physics/coordinates.py
@@ -0,0 +1,32 @@
+import math
+
+
+def cartesian_to_polar(x, y):
+    r_square = x*x + y*y
+    r = math.sqrt(r_square)
+    theta = math.atan2(y, x)
+    return r, theta
+
+def polar_to_cartesian(r, theta):
+    x = r * math.cos(theta)
+    y = r * math.sin(theta)
+    return x, y
+
+def cartesian_to_cylindrical(x, y, z):
+    r, theta = cartesian_to_polar(x, y)
+    return r, theta, z
+
+def cylindrical_to_cartesian(r, theta, z):
+    x, y = polar_to_cartesian(r, theta)
+    return x, y, z
+
+def cartesian_to_spherical(x, y, z):
+    d, phi = cartesian_to_polar(x, y)
+    r, theta = cartesian_to_polar(z, d)
+    return r, phi, theta
+
+def spherical_to_cartesian(r, phi, theta):
+    z, d = polar_to_cartesian(r, theta)
+    x, y = polar_to_cartesian(d, phi)
+    return x, y, z
+
